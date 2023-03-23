04e60d7c9b9d4ffe40fb672582fe5075ddee19d8
Houseliofer <20030392@itcelaya.edu.mx>
2023-03-22 19:55:56 -0600

    Agrege Interfaz no funciona

---
+++ src/main/java/org/main/frmPassword.java
@@ -0,0 +1,11 @@
+package org.main;
+
+import javax.swing.*;
+
+public class frmPassword {
+    private JTextField textField1;
+    private JButton enviarButton;
+    private JButton reiniciarButton;
+
+
+}
